bebc406b516ec3a7b4a12855833a63db19f00b80
liapino1 <l.ancavilpno@gmail.com>
2019-09-28 20:10:07 -0300

    Fixed phone in models authentication :hankey:

--- authentication/models.py
+++ authentication/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     user = models.OneToOneField(User,null=True, default=None, on_delete=models.CASCADE)
     address = models.CharField(max_length = 30)
-    phone = models.IntegerField()
+    phone = models.PositiveIntegerField()
 
     def __str__(self):
         return "Usuario: %s" % (self.user)
