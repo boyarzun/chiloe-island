884b4fe6f2e8d7a4e80210e6e0a0e6159f328e58
Byron Oyarzún <b.oyarzn@gmail.com>
2019-09-27 23:47:59 -0300

    Add templates

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from authentication import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.welcome),
+    path('register', views.register),
+    path('login', views.login),
+    path('logout', views.logout),
 ]
