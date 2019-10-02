f6e6be3f658f4e719f7bda8c9be42c8c246699bb
Byron Oyarzún <b.oyarzn@gmail.com>
2019-10-01 23:22:41 -0300

    Add DataTable server side processing

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from users.views import index
+from users.views import index, get_users_list
 
 app_name = 'users'
 
 urlpatterns = [
     path('users', index, name='index'),
+    path('users/list', get_users_list, name='datatable')
 ]
 
--- users/views.py
+++ users/views.py
@@ -1,8 +1,54 @@
-from django.shortcuts import render
 
+# Django
+from django.core.paginator import Paginator
+from django.shortcuts import render
+from django.http import JsonResponse
+
+# Models
+from authentication.models import UserProfile
+
+# Utils
+from dashboard.utils import DataTable
+
+def get_users_list(request):
+    """ API for load data in DataTable """
+    
+    dt = DataTable(request)    
+    users_list = UserProfile.objects.filter(user__first_name__contains = dt.search) | UserProfile.objects.filter(user__last_name__contains = dt.search)
+
+    # Pagination
+    paginator = Paginator(list(users_list), 15)
+    users = paginator.get_page(dt.start)
+
+    # Prepare data
+    def sort_data(user_profile):
+        return [
+            user_profile.user.username,
+            user_profile.user.first_name + ' ' + user_profile.user.last_name,
+            user_profile.user.email,
+            user_profile.address,
+            user_profile.phone
+        ]
+
+    data_users = list(map(sort_data, users.object_list))
+
+    data = {
+        "draw": dt.draw,
+        "recordsTotal": paginator.count,
+        "recordsFiltered": paginator.count,
+        "data": data_users
+    }
+
+    return JsonResponse(data, safe=False)
 
 def index(request):
-    context = { 'menu': 'users'}
+
+    total_users = UserProfile.objects.all().count()
+
+    context = {
+        'total_users': total_users,
+        'menu': 'users',
+        }
     return render(request, 'users/index.html', context)
 
 def create(request):
