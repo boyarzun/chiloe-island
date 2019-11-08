df8a2015e98613e9a4934bf748dcc01b17c34621
Byron Oyarzún <b.oyarzn@gmail.com>
2019-11-08 16:38:09 -0300

    Connection between view and template

--- settings/models.py
+++ settings/models.py
@@ -20,5 +20,28 @@ class Setting(models.Model):
     description = models.CharField(max_length=240)
     keywords = models.CharField(max_length=240)
 
+    # Social networks
+    instagram = models.CharField(max_length=128, blank=True)
+    twitter = models.CharField(max_length=128, blank=True)
+    facebook = models.CharField(max_length=128, blank=True)
+
+    # Template settings
+
+    ## Menu options
+    menu_color = models.PositiveSmallIntegerField()
+    menu_dark = models.BooleanField()
+    menu_collapsed = models.BooleanField()
+    menu_selection = models.PositiveSmallIntegerField()
+
+    ## Navbar options
+    navbar_color = models.PositiveSmallIntegerField()
+    navbar_dark = models.BooleanField()
+    navbar_fixed = models.BooleanField()
+
+    ## Footer options
+    footer_dark = models.BooleanField()
+    footer_fixed = models.BooleanField()
+
+
     def __str__(self):
         return self.name
--- settings/urls.py
+++ settings/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from settings.views import index
+from settings.views import index, template_options
 
 app_name = 'settings'
 
 urlpatterns = [
     path('settings', index, name='index'),
+    path('settings/template', template_options, name='customizer'),
 ]
 
