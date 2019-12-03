ab89ade7227d8bce4f305424bb95c69b31fb3ebc
Byron Oyarzún <b.oyarzn@gmail.com>
2019-12-02 23:15:04 -0300

    Added utils js to gilajan template

---
+++ core/static/assets/js/utils.js
@@ -0,0 +1,5 @@
+function addUrlParameter(name, value) {
+    var searchParams = new URLSearchParams(window.location.search)
+    searchParams.set(name, value)
+    window.location.search = searchParams.toString()
+}
