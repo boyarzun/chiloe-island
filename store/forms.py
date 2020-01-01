36295026175fd4106fd0f5dec5fb7a7bacb9a9d2
Byron Oyarzún <b.oyarzn@gmail.com>
2020-01-01 13:11:39 -0300

    Updated delete image from edit form

--- store/forms.py
+++ store/forms.py
@@ -5,6 +5,22 @@ from django import forms
 from store.models import Product
 
 class ProductForm(forms.ModelForm):
+
+    images_status = forms.CharField(max_length=20, widget=forms.HiddenInput(), initial='0::0::0')
+
     class Meta:
         model = Product
         exclude = ('pk',)
+
+    def is_valid(self):
+        """Return True if the form has no errors, or False otherwise."""
+        splittedValue = self.data['images_status'].split('::')
+
+        if (splittedValue[0] == '1'):
+            self.initial.image_one.delete()
+        elif (splittedValue[1] == '1'):
+            self.initial.image_two.delete()
+        elif (splittedValue[2] == '1'):
+            self.initial.image_three.delete()
+
+        return self.is_bound and not self.errors
