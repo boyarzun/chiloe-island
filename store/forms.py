1980920d484fa569e199ff483b6f83d2c2eb6cb4
Byron Oyarzún <b.oyarzn@gmail.com>
2020-01-01 13:40:10 -0300

    Updated delete image from edit form

--- store/forms.py
+++ store/forms.py
@@ -14,13 +14,15 @@ class ProductForm(forms.ModelForm):
 
     def is_valid(self):
         """Return True if the form has no errors, or False otherwise."""
-        splittedValue = self.data['images_status'].split('::')
 
-        if (splittedValue[0] == '1'):
-            self.initial.image_one.delete()
-        elif (splittedValue[1] == '1'):
-            self.initial.image_two.delete()
-        elif (splittedValue[2] == '1'):
-            self.initial.image_three.delete()
+        if self.initial:
+            splittedValue = self.data['images_status'].split('::')
+
+            if (splittedValue[0] == '1'):
+                self.initial['image_one'].delete()
+            elif (splittedValue[1] == '1'):
+                self.initial['image_two'].delete()
+            elif (splittedValue[2] == '1'):
+                self.initial['image_three'].delete()
 
         return self.is_bound and not self.errors
