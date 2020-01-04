a65a8f1c173f950da13d328690239dd15f852785
Byron Oyarzún <b.oyarzn@gmail.com>
2020-01-04 15:49:12 -0300

    Update userprofile model

--- authentication/models.py
+++ authentication/models.py
@@ -15,19 +15,28 @@ class Commune(models.Model):
         return self.name
 
 def user_directory_path(instance, filename):
-    """ File will be uploaded to MEDIA_ROOT/<user_id>/<filename> """
+    """ File will be uploaded to MEDIA_ROOT/users/<user_id>/products/<filename> """
 
     ext = filename.split('.')[-1]
     name = uuid.uuid4()
-    return f'users/{instance.user.id}/{name}.{ext}'
+    return f'users/{instance.user.id}/products/{name}.{ext}'
+
+def cover_photo_directory_path(instance, filename):
+    """ File will be uploaded to MEDIA_ROOT/users/<user_id>/cover/<filename> """
+
+    ext = filename.split('.')[-1]
+    name = uuid.uuid4()
+    return f'users/{instance.user.id}/cover/{name}.{ext}'
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     address = models.CharField(max_length = 50, null=True, verbose_name="Dirección")
     phone = models.CharField(max_length=18, null=True, verbose_name="Teléfono")
     avatar = models.ImageField(upload_to=user_directory_path, default='avatar/default.png')
+    cover_photo = models.ImageField(upload_to=cover_photo_directory_path, null=True, blank=True)
     map = models.URLField(max_length=250, null=True, blank=True)
     commune = models.ForeignKey(Commune, on_delete=models.CASCADE)
+    terms_conditions= models.BooleanField(default=True)
 
     def __str__(self):
         return "Usuario: %s" % (self.user)
