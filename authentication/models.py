77215c354deb82335acb1984139b93dd584cd506
Byron Oyarzún <b.oyarzn@gmail.com>
2020-01-04 22:00:26 -0300

    Updated max_lenght commune

--- authentication/models.py
+++ authentication/models.py
@@ -9,9 +9,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Commune(models.Model):
-    name = models.CharField(max_length=15)
-    image = models.CharField(max_length=15)
-    slug = models.SlugField(max_length=15, verbose_name='Nombre slug')
+    name = models.CharField(max_length=18)
+    image = models.CharField(max_length=18)
+    slug = models.SlugField(max_length=18, verbose_name='Nombre slug')
 
     def __str__(self):
         return self.name
