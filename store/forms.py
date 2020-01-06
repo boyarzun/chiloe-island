fecf318b2d34628f1597a17ee734f03f8d441c39
Byron Oyarzún <b.oyarzn@gmail.com>
2020-01-06 16:31:56 -0300

    Fixed delete images from edit form

--- store/forms.py
+++ store/forms.py
@@ -6,12 +6,23 @@ from store.models import Product
 
 class ProductForm(forms.ModelForm):
 
+    images_status = forms.CharField(max_length=20, widget=forms.HiddenInput(), initial='0::0::0::0')
+
     class Meta:
         model = Product
         exclude = ('pk',)
 
     def is_valid(self):
         """Return True if the form has no errors, or False otherwise."""
+
+        splittedValue = self.data['images_status'].split('::')
+
+        if (splittedValue[1] == '1'):
+            self.initial['image_two'].delete()
+        if (splittedValue[2] == '1'):
+            self.initial['image_three'].delete()
+        if (splittedValue[3] == '1'):
+            self.initial['image_four'].delete()
 
         if self.files and self.initial:
             for key in self.files:
