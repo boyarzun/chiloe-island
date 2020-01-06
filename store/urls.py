2523c563ba0882c31150d6c27f86a56bca1fdef0
Byron Oyarzún <b.oyarzn@gmail.com>
2020-01-05 21:45:44 -0300

    Fixed wrong urls

--- store/urls.py
+++ store/urls.py
@@ -4,9 +4,11 @@ from store.views import *
 app_name = 'store'
 
 urlpatterns = [
+    # Public store
     path('store', index, name="index"),
     path('store/<int:id_product>', seller_product, name="sellerproduct"),
     path('store/<slug:store_name>', seller_store, name="sellerstore"),
+    # Dashboard
     path('mystore/', mystore_index, name="mystore"),
     path('mystore/new', products_create, name="new"),
     path('mystore/<int:product_id>', product_edit, name="edit"),
