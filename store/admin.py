9cfabb1d9f083dea70063f42abf05956cfa41d2d
Byron Oyarzún <b.oyarzn@gmail.com>
2020-01-16 21:10:38 +0000

    Added Category and SubCategory models to admin

--- store/admin.py
+++ store/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from store.models import Product
+from store.models import Product, Category, SubCategory
 
 admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(SubCategory)
