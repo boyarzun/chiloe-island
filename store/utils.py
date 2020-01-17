368e373adde528350c9e214f8df049f5085f1261
Byron Oyarzún <b.oyarzn@gmail.com>
2020-01-17 18:44:45 -0300

    Added distinct() to utils store

--- store/utils.py
+++ store/utils.py
@@ -42,7 +42,7 @@ def index_filter(request, seller=None):
     # epp = elements per page
     epp = 16
     page = request.GET.get('page')
-    paginator = Paginator(list(products_list), epp)
+    paginator = Paginator(list(products_list.distinct()), epp)
     products = paginator.get_page(page)
 
 
